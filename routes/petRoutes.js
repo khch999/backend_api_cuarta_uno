71be0ca8d4fd2925ab8c7588ac4632b6a9560718
khch999 <kevinjherrerach999@gmail.com>
2026-02-11 02:51:46 -0600

    fix middleware ruta mascotas

--- routes/petRoutes.js
+++ routes/petRoutes.js
@@ -2,7 +2,7 @@ const express = require('express');
 const router = express.Router();
 const PetController = require('../controllers/petController');
 const {validatePetId} = require('../middleware/validation');
-const authenticateToken = require('../middleware/auth');
+const {authenticateToken} = require('../middleware/auth');
 
 /**
  * @route GET /pets/my
